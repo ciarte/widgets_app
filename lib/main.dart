d91f3da85ed7325db67fee2af6913d163f61ad60
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-07-24 21:10:36 -0300

    go_router configurado

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
-import 'presentation/screens/buttons/buttons_screen.dart';
-import 'presentation/screens/cards/cards_screen.dart';
-import 'presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,14 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router (
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 2).getTheme(),
-      home: const HomeScreen(),
-      routes: {
-        '/buttons': (context) => const ButtonsScreen(),
-        '/cards': (context) => const CardsScreen(),
-      },
     );
   }
 }
